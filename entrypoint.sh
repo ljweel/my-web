5c7cc4c3f8360da1218961021706a69d4152e4bd
JEONGWOO LEE <ljwljw8541@uos.ac.kr>
2025-12-02 12:09:40 +0900

    feat: live reload

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "PostgreSQL is up"
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 5 --reload

adf6ff70ddb86aaac2e89e03c23104b0a596f615
JEONGWOO LEE <ljwljw8541@uos.ac.kr>
2025-12-10 16:06:25 +0900

    refactor: Add verbose_name in Post model

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,20 @@ from django.db import models
 from django.conf import settings
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, 
+        verbose_name='작성자', 
+        related_name='user_posts'
+    )
+    title = models.CharField(max_length=100, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+
+    class Meta:
+        ordering = ['created_at'] 
+        verbose_name = '게시글'
+    
     def __str__(self):
         return self.title
